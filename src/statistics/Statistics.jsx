60f8b70d6bd5bedb03fc05bc603d51ec3d64c811
Lightlawn <lightlawn@gmail.com>
2021-09-04 16:52:22 +0200

    fixes code mistakes

--- src/statistics/Statistics.jsx
+++ src/statistics/Statistics.jsx
@@ -1,15 +1,15 @@
 import PropTypes, { shape } from 'prop-types';
 import styles from './statistics.module.css';
 
-function Statistics({stats}) {
+function Statistics({stats, title}) {
   return (
     <section className={styles.statistics}>
-        <h2 className={styles.title}>Upload stats</h2>
+      { title && <h2 className={styles.title}>Upload stats</h2>}
 
         <ul className={styles.statsList}>
             {stats.map(data => 
                 <li className={styles.item}>
-                    {data.label ? <span className={styles.label}>{data.label} </span> : <span></span>}
+                    <span className={styles.label}>{data.label} </span>
                     <span className={styles.percentage}>{data.percentage}%</span>
                 </li>
             )}
